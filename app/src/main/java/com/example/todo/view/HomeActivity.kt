9d521a67256bbf2ff7910d10fa759741a21bf1cc
Eda Dursun <edadrsn929@gmail.com>
2025-05-11 22:58:14 +0300

    create task function created

--- app/src/main/java/com/example/todo/view/HomeActivity.kt
+++ app/src/main/java/com/example/todo/view/HomeActivity.kt
@@ -1,28 +1,38 @@
-package com.example.todo
+package com.example.todo.view
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todo.adapter.TaskRecyclerAdapter
 import com.example.todo.databinding.ActivityHomeBinding
+import com.example.todo.model.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityHomeBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
-
-
-    fun createTask(view: View){
-        val intent= Intent(this@HomeActivity,CreateTaskActivity::class.java)
+    
+    //Görev oluştur butonu tanımlandı
+    fun createTask(view: View) {
+        val intent = Intent(this@HomeActivity, CreateTaskActivity::class.java)
         startActivity(intent)
     }
+
 }
