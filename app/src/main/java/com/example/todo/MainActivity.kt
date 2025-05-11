f51d2eb9b259f490e80320a27043feb13c0bbc29
Eda Dursun <edadrsn929@gmail.com>
2025-05-11 18:33:38 +0300

    authentication process completed

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,34 +3,97 @@ package com.example.todo
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.todo.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    fun getEmailAndPassword(): Pair<String, String>? {
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(this@MainActivity, "Please enter email and password", Toast.LENGTH_SHORT)
+                .show()
+            return null
+        }
+
+        return Pair(email, password)
+    }
+
+    fun signIn(view: View) {
+        val receiveInfo = getEmailAndPassword() ?: return
+        val (email, password) = receiveInfo
+
+        if (password.length < 6) {
+            Toast.makeText(
+                this@MainActivity,
+                "Password must be least 6 characters",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
     }
 
 
-    fun signIn(view: View){
+    fun signUp(view: View) {
+        val receiveInfo = getEmailAndPassword() ?: return
+        val (email, password) = receiveInfo
 
-        val intent=Intent(this@MainActivity,HomeActivity::class.java)
-        startActivity(intent )
+        if (password.length < 6) {
+            Toast.makeText(
+                this@MainActivity,
+                "Password must be least 6 characters",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
 
-    }
-
-
-    fun signUp(view:View){
-
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 }
