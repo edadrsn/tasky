088e741c6025600dcc87076abb5e4dca0e902cb7
Eda Dursun <edadrsn929@gmail.com>
2025-05-14 22:08:54 +0300

    Task model updated

--- app/src/main/java/com/example/todo/model/Task.kt
+++ app/src/main/java/com/example/todo/model/Task.kt
@@ -1,6 +1,10 @@
 package com.example.todo.model
 
+import com.google.type.Date
+
 data class Task(
     val taskId:String="",
-    val task:String
+    val task:String="",
+    val taskDescription:String="",
+    val taskDate: java.util.Date
 )
