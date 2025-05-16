9caf1c520f99b1da40f68df45a66c1972596cb7d
Eda Dursun <edadrsn929@gmail.com>
2025-05-16 22:36:20 +0300

    Detail task activity is created and data from the intent is received

--- app/src/main/java/com/example/todo/view/DetailTaskActivity.kt
+++ app/src/main/java/com/example/todo/view/DetailTaskActivity.kt
@@ -6,16 +6,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.todo.R
+import com.example.todo.databinding.ActivityDetailTaskBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DetailTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivityDetailTaskBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail_task)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityDetailTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Verileri al
+        val taskTitle=intent.getStringExtra("taskTitle")
+        val taskDescription=intent.getStringExtra("taskDescription")
+        val taskDateMillis=intent.getLongExtra("taskDate",0L)
+        val taskDate= Date(taskDateMillis)
+
+        //Aldığın verileri yazdır
+        binding.detailTaskTitle.text=taskTitle
+        binding.detailTaskDescription.text=taskDescription
+        binding.detailTaskDate.text= SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(taskDate)
+
     }
 }
